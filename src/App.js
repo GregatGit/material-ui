fb88bf8898e76bdbfc75c89236b0f47ff366e6a6
gregatgit <gregdd@outlook.com>
2020-06-04 11:39:42 +1000

    basic content component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Header from './components/Header'
+import Content from './components/Content'
 
 function App() {
   
@@ -9,7 +10,9 @@ function App() {
       <Grid item><Header /></Grid>
       <Grid item container>
         <Grid item xs={0} sm={2} />
-        <Grid item xs={12} sm={8}>Blah Blah Blah this is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isvthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content is</Grid>
+        <Grid item xs={12} sm={8}>
+        <Content />
+        </Grid>
         <Grid item xs={0} sm={2} />
       </Grid>
     </Grid>
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div>
+    Blah Blah Blah this is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isvthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content isthis is where the content is
+    </div>
+  )
+}
+
+export default Content
